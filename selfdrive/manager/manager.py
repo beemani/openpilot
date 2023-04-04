f6776804ca1417c43a9e551c3e3256235473a254
royjr <royjr96@gmail.com>
2023-04-04 11:00:36 -0400

    set default

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -41,6 +41,7 @@ def manager_init() -> None:
     ("HasAcceptedTerms", "0"),
     ("LanguageSetting", "main_en"),
     ("OpenpilotEnabledToggle", "1"),
+    ("SEMIPILOT_distance", "3"),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
