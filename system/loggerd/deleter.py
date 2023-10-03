aa6cb961f4f52a21bed8e78adf9c02f9f30da883
royjr <royjr96@gmail.com>
2023-10-03 00:03:40 -0400

    Update deleter.py

--- system/loggerd/deleter.py
+++ system/loggerd/deleter.py
@@ -8,6 +8,7 @@ from openpilot.system.swaglog import cloudlog
 from openpilot.system.loggerd.config import get_available_bytes, get_available_percent
 from openpilot.system.loggerd.uploader import listdir_by_creation
 from openpilot.system.loggerd.xattr_cache import getxattr
+from openpilot.system.loggerd.external import is_mounted
 
 MIN_BYTES = 5 * 1024 * 1024 * 1024
 MIN_PERCENT = 10
@@ -47,32 +48,72 @@ def get_preserved_segments(dirs_by_creation: List[str]) -> List[str]:
 
 def deleter_thread(exit_event):
   while not exit_event.is_set():
-    out_of_bytes = get_available_bytes(default=MIN_BYTES + 1) < MIN_BYTES
-    out_of_percent = get_available_percent(default=MIN_PERCENT + 1) < MIN_PERCENT
+    out_of_bytes_internal = get_available_bytes(path_type="internal", default=MIN_BYTES + 1) < MIN_BYTES
+    out_of_percent_internal = get_available_percent(path_type="internal", default=MIN_PERCENT + 1) < MIN_PERCENT
 
-    if out_of_percent or out_of_bytes:
-      dirs = listdir_by_creation(Paths.log_root())
+    internal_path = PATH_DICT["internal"]
+    external_path = PATH_DICT["external"]
 
-      # skip deleting most recent N preserved segments (and their prior segment)
-      preserved_dirs = get_preserved_segments(dirs)
+    if out_of_percent_internal or out_of_bytes_internal:
+      # Check if the external mount is alive.
+      if is_mounted(external_path):
+        out_of_bytes_external = get_available_bytes(path_type="external", default=MIN_BYTES + 1) < MIN_BYTES
+        out_of_percent_external = get_available_percent(path_type="external", default=MIN_PERCENT + 1) < MIN_PERCENT
 
-      # remove the earliest directory we can
-      for delete_dir in sorted(dirs, key=lambda d: (d in DELETE_LAST, d in preserved_dirs)):
-        delete_path = os.path.join(Paths.log_root(), delete_dir)
+        # If the external storage is out of space, delete from it.
+        if out_of_bytes_external or out_of_percent_external:
+          dirs = listdir_by_creation(external_path)
+          preserved_dirs = get_preserved_segments(dirs)
+          for delete_dir in sorted(dirs, key=lambda d: (d in DELETE_LAST, d in preserved_dirs)):
+            delete_path = os.path.join(external_path, delete_dir)
+            if any(name.endswith(".lock") for name in os.listdir(delete_path)):
+              continue
+            try:
+              cloudlog.info(f"deleting {delete_path}")
+              if os.path.isfile(delete_path):
+                os.remove(delete_path)
+              else:
+                shutil.rmtree(delete_path)
+              break
+            except OSError:
+              cloudlog.exception(f"issue deleting {delete_path}")
+          exit_event.wait(.1)
 
-        if any(name.endswith(".lock") for name in os.listdir(delete_path)):
-          continue
+        # Move data from internal to external.
+        dirs = listdir_by_creation(internal_path)
+        preserved_dirs = get_preserved_segments(dirs)
+        for move_dir in sorted(dirs, key=lambda d: (d in DELETE_LAST, d in preserved_dirs)):
+          move_from = os.path.join(internal_path, move_dir)
+          move_to = os.path.join(external_path, move_dir)
 
-        try:
-          cloudlog.info(f"deleting {delete_path}")
-          if os.path.isfile(delete_path):
-            os.remove(delete_path)
-          else:
-            shutil.rmtree(delete_path)
-          break
-        except OSError:
-          cloudlog.exception(f"issue deleting {delete_path}")
-      exit_event.wait(.1)
+          if any(name.endswith(".lock") for name in os.listdir(move_from)):
+            continue
+          try:
+            cloudlog.info(f"moving {move_from} to {move_to}")
+            shutil.move(move_from, move_to)
+            break
+          except Exception as e:
+            cloudlog.exception(f"issue moving {move_from} to {move_to}: {str(e)}")
+        exit_event.wait(.1)
+
+      # If external storage is not mounted, delete from internal storage.
+      else:
+        dirs = listdir_by_creation(internal_path)
+        preserved_dirs = get_preserved_segments(dirs)
+        for delete_dir in sorted(dirs, key=lambda d: (d in DELETE_LAST, d in preserved_dirs)):
+          delete_path = os.path.join(internal_path, delete_dir)
+          if any(name.endswith(".lock") for name in os.listdir(delete_path)):
+            continue
+          try:
+            cloudlog.info(f"deleting {delete_path}")
+            if os.path.isfile(delete_path):
+              os.remove(delete_path)
+            else:
+              shutil.rmtree(delete_path)
+            break
+          except OSError:
+            cloudlog.exception(f"issue deleting {delete_path}")
+        exit_event.wait(.1)
     else:
       exit_event.wait(30)
 
